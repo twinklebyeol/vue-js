491eb9b2ed6d6f4125c7e42bb7e505aa3478e057
twinklebyeol <131242674+twinklebyeol@users.noreply.github.com>
2023-06-22 18:20:06 +0900

    dd

--- vue_js/vuetify_project2/vuetify_project2/src/store/index.js
+++ vue_js/vuetify_project2/vuetify_project2/src/store/index.js
@@ -4,22 +4,18 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    sTitle: "",
-  },
-
-  mutations: {
-    fnSetData(state, payload) {
-      state.sTitle = payload;
-    },
-  },
+  state: { sTitle: "" },
 
   getters: {
     fnGetData(state) {
       return state.sTitle;
     },
   },
-
+  mutations: {
+    fnSetData: function (state, payload) {
+      return (state.sTitle = payload); // 엉니 왜 리턴..아~
+    },
+  },
   actions: {},
   modules: {},
 });
